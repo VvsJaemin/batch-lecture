4d352648f14019d23a31561a0914d62a878347b8
VvsJaemin <djd453216@nate.com>
2022-11-24 21:26:04 +0900

    feat : Spring batch jobInstance, jobExcution

---
+++ src/main/java/io/springbatch/springbatchlecture/JobParameterTest.java
@@ -0,0 +1,29 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JobParameterTest implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher;
+
+    private final Job job;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+
+
+    }
+}
