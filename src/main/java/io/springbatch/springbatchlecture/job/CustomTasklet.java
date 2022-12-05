c672938991aa008462cf540ff9b4e057a052e1d1
VvsJaemin <djd453216@nate.com>
2022-12-05 21:42:50 +0900

    feat : Spring batch 초기화 설정

--- src/main/java/io/springbatch/springbatchlecture/job/CustomTasklet.java
+++ src/main/java/io/springbatch/springbatchlecture/job/CustomTasklet.java
@@ -1,18 +1,18 @@
-package io.springbatch.springbatchlecture.job;
-
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomTasklet implements Tasklet {
-    @Override
-    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-
-        System.out.println("step1 was executed");
-
-        return RepeatStatus.FINISHED;
-    }
-}
+//package io.springbatch.springbatchlecture.job;
+//
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CustomTasklet implements Tasklet {
+//    @Override
+//    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//
+//        System.out.println("step1 was executed");
+//
+//        return RepeatStatus.FINISHED;
+//    }
+//}
--- src/main/java/io/springbatch/springbatchlecture/jobLauncher/Member.java
+++ src/main/java/io/springbatch/springbatchlecture/jobLauncher/Member.java
@@ -1,9 +1,9 @@
-package io.springbatch.springbatchlecture.jobLauncher;
-
-import lombok.Data;
-
-@Data
-public class Member {
-
-    private String id;
-}
+//package io.springbatch.springbatchlecture.jobLauncher;
+//
+//import lombok.Data;
+//
+//@Data
+//public class Member {
+//
+//    private String id;
+//}
