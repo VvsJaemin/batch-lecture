0c63b9844974fa9468d23b7431e8a49cec58d7bf
VvsJaemin <djd453216@nate.com>
2022-12-08 22:56:33 +0900

    feat : Spring batch simplejob = start(), next()

--- src/main/java/io/springbatch/springbatchlecture/job/SimpleJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/job/SimpleJobConfiguration.java
@@ -1,86 +1,86 @@
-package io.springbatch.springbatchlecture.job;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RequiredArgsConstructor
-public class SimpleJobConfiguration {
-
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job batchJob() {
-        return jobBuilderFactory.get("batchJob")
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .incrementer(new RunIdIncrementer())
-                .validator(new JobParametersValidator() {
-                    @Override
-                    public void validate(JobParameters parameters) throws JobParametersInvalidException {
-
-                    }
-                })
-                .preventRestart()
-                .listener(new JobExecutionListener() {
-                    @Override
-                    public void beforeJob(JobExecution jobExecution) {
-
-                    }
-
-                    @Override
-                    public void afterJob(JobExecution jobExecution) {
-
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(((contribution, chunkContext) -> {
-                    System.out.println("step1 was executed ");
-                    return RepeatStatus.FINISHED;
-                }))
-                .build();
-    }
-
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(((contribution, chunkContext) -> {
-                    System.out.println("step2 was executed ");
-                    return RepeatStatus.FINISHED;
-                }))
-                .build();
-    }
-
-
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(((contribution, chunkContext) -> {
-                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-                    contribution.setExitStatus(ExitStatus.STOPPED);
-                    System.out.println("step3 was executed ");
-                    return RepeatStatus.FINISHED;
-                }))
-                .build();
-    }
-
-
-
-}
+//package io.springbatch.springbatchlecture.job;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.*;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.job.builder.FlowBuilder;
+//import org.springframework.batch.core.job.flow.Flow;
+//import org.springframework.batch.core.launch.support.RunIdIncrementer;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class SimpleJobConfiguration {
+//
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job batchJob() {
+//        return jobBuilderFactory.get("batchJob")
+//                .start(step1())
+//                .next(step2())
+//                .next(step3())
+//                .incrementer(new RunIdIncrementer())
+//                .validator(new JobParametersValidator() {
+//                    @Override
+//                    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+//
+//                    }
+//                })
+//                .preventRestart()
+//                .listener(new JobExecutionListener() {
+//                    @Override
+//                    public void beforeJob(JobExecution jobExecution) {
+//
+//                    }
+//
+//                    @Override
+//                    public void afterJob(JobExecution jobExecution) {
+//
+//                    }
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//                .tasklet(((contribution, chunkContext) -> {
+//                    System.out.println("step1 was executed ");
+//                    return RepeatStatus.FINISHED;
+//                }))
+//                .build();
+//    }
+//
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//                .tasklet(((contribution, chunkContext) -> {
+//                    System.out.println("step2 was executed ");
+//                    return RepeatStatus.FINISHED;
+//                }))
+//                .build();
+//    }
+//
+//
+//
+//    @Bean
+//    public Step step3() {
+//        return stepBuilderFactory.get("step3")
+//                .tasklet(((contribution, chunkContext) -> {
+//                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+//                    contribution.setExitStatus(ExitStatus.STOPPED);
+//                    System.out.println("step3 was executed ");
+//                    return RepeatStatus.FINISHED;
+//                }))
+//                .build();
+//    }
+//
+//
+//
+//}
---
+++ src/main/java/io/springbatch/springbatchlecture/job/StartNextConfiguration.java
@@ -0,0 +1,62 @@
+package io.springbatch.springbatchlecture.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class StartNextConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println("step1 was executed ");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println("step2 was executed ");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(((contribution, chunkContext) -> {
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+
+
+}
