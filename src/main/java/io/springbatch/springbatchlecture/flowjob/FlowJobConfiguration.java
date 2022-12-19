51cb9258e7ab05580047e1c33f7dbbb564c94fa3
VvsJaemin <djd453216@nate.com>
2022-12-19 22:32:49 +0900

    spring batch flowjob - start(), next()

--- src/main/java/io/springbatch/springbatchlecture/flowjob/FlowJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/flowjob/FlowJobConfiguration.java
@@ -1,73 +1,73 @@
-package io.springbatch.springbatchlecture.flowjob;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RequiredArgsConstructor
-public class FlowJobConfiguration {
-
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job batchJob() {
-        return jobBuilderFactory.get("step1")
-                .start(step1())
-                .on("COMPLETED").to(step3())
-                .from(step1())
-                .on("FAILED").to(step2())
-                .end()
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(" step 1 has executed");
-//                        throw new RuntimeException("step1 was failed");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
-
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(" step 2 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(" step 3 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
-}
-
+//package io.springbatch.springbatchlecture.flowjob;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.beans.factory.annotation.Configurable;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class FlowJobConfiguration {
+//
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job batchJob() {
+//        return jobBuilderFactory.get("step1")
+//                .start(step1())
+//                .on("COMPLETED").to(step3())
+//                .from(step1())
+//                .on("FAILED").to(step2())
+//                .end()
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println(" step 1 has executed");
+////                        throw new RuntimeException("step1 was failed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }).build();
+//    }
+//
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println(" step 2 has executed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }).build();
+//    }
+//
+//    @Bean
+//    public Step step3() {
+//        return stepBuilderFactory.get("step3")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println(" step 3 has executed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }).build();
+//    }
+//}
+//
