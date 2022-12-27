0df91eff0a2b089eeac8e744a54c6ade02ff44c0
VvsJaemin <djd453216@nate.com>
2022-12-27 21:30:22 +0900

    feat : FlowJobConfiguration

--- src/main/java/io/springbatch/springbatchlecture/jobexecution/CustomDecider.java
+++ src/main/java/io/springbatch/springbatchlecture/jobexecution/CustomDecider.java
@@ -1,19 +1,19 @@
-package io.springbatch.springbatchlecture.jobexecution;
-
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.job.flow.FlowExecutionStatus;
-import org.springframework.batch.core.job.flow.JobExecutionDecider;
-
-public class CustomDecider implements JobExecutionDecider {
-    private int count = 0;
-    @Override
-    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
-        count++;
-        if (count % 2 == 0) {
-            return new FlowExecutionStatus("EVEN");
-        } else {
-            return new FlowExecutionStatus("ODD");
-        }
-    }
-}
+//package io.springbatch.springbatchlecture.jobexecution;
+//
+//import org.springframework.batch.core.JobExecution;
+//import org.springframework.batch.core.StepExecution;
+//import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+//import org.springframework.batch.core.job.flow.JobExecutionDecider;
+//
+//public class CustomDecider implements JobExecutionDecider {
+//    private int count = 0;
+//    @Override
+//    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+//        count++;
+//        if (count % 2 == 0) {
+//            return new FlowExecutionStatus("EVEN");
+//        } else {
+//            return new FlowExecutionStatus("ODD");
+//        }
+//    }
+//}
