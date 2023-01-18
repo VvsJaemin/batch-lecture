6a137176a4e02e54c55fe971eee9bbbbe4f3c9d1
VvsJaemin <djd453216@nate.com>
2023-01-18 22:15:44 +0900

    feat : Spring batch JsonItemReader

--- src/main/java/io/springbatch/springbatchlecture/flatfile/Customer.java
+++ src/main/java/io/springbatch/springbatchlecture/flatfile/Customer.java
@@ -1,11 +1,11 @@
-package io.springbatch.springbatchlecture.flatfile;
-
-import lombok.Data;
-
-@Data
-public class Customer {
-
-    private String name;
-    private int age;
-    private String year;
-}
+//package io.springbatch.springbatchlecture.flatfile;
+//
+//import lombok.Data;
+//
+//@Data
+//public class Customer {
+//
+//    private String name;
+//    private int age;
+//    private String year;
+//}
---
+++ src/main/java/io/springbatch/springbatchlecture/json/JsonConfiguration.java
@@ -0,0 +1,67 @@
+package io.springbatch.springbatchlecture.json;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class JsonConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+
+    @Bean
+    @JobScope
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(3) //<input, output>
+                .reader(customItemReader())
+                .writer(customItemWriter())
+                .build();
+    }
+    @Bean
+    public ItemReader<? extends Customer> customItemReader() {
+        return new JsonItemReaderBuilder<Customer>()
+                .name("jsonReader")
+                .resource(new ClassPathResource("customer.json"))
+                .jsonObjectReader(new JacksonJsonObjectReader<>(Customer.class))
+                .build();
+    }
+
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println("item.toString() = " + item.toString());
+            }
+        };
+    }
+
+
+}
+
