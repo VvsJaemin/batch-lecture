5df7b715f723e812967d1cb6a281a8e188b75ba2
VvsJaemin <djd453216@nate.com>
2023-01-20 13:01:45 +0900

    feat : Spring batch JdbcCursorItemReader

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,10 @@
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (1, "test10", "test20","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (11, "test11", "test21","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (23, "test12", "test22","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (14, "test13", "test23","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (15, "test14", "test24","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (16, "test15", "test25","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (17, "test16", "test26","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (18, "test17", "test27","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (19, "test18", "test28","1952-07-16 12:34:53");
+insert into `customer` (`id`, `firstName`, `lastName`, `birthdate`) values (20, "test19", "test29","1952-07-16 12:34:53");
