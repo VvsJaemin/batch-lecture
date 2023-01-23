6af6b1c29593d59731e34420fb8d38c3536375eb
VvsJaemin <djd453216@nate.com>
2023-01-23 11:16:25 +0900

    feat : Spring batch FlatFileItemWriter(delimeted, format)

--- src/main/java/io/springbatch/springbatchlecture/flatfile/Customer.java
+++ src/main/java/io/springbatch/springbatchlecture/flatfile/Customer.java
@@ -1,11 +1,13 @@
-//package io.springbatch.springbatchlecture.flatfile;
-//
-//import lombok.Data;
-//
-//@Data
-//public class Customer {
-//
-//    private String name;
-//    private int age;
-//    private String year;
-//}
+package io.springbatch.springbatchlecture.flatfile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+
+    private long id;
+    private String name;
+    private int age;
+}
--- src/main/java/io/springbatch/springbatchlecture/flatfile/FlatFilesConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/flatfile/FlatFilesConfiguration.java
@@ -1,95 +1,82 @@
-//package io.springbatch.springbatchlecture.flatfile;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.batch.core.Job;
-//import org.springframework.batch.core.Step;
-//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-//import org.springframework.batch.core.configuration.annotation.JobScope;
-//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-//import org.springframework.batch.item.ItemWriter;
-//import org.springframework.batch.item.file.FlatFileItemReader;
-//import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-//import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-//import org.springframework.batch.item.file.transform.Range;
-//import org.springframework.batch.repeat.RepeatStatus;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.FileSystemResource;
-//
-//import java.util.List;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class FlatFilesConfiguration {
-//
-//    private final JobBuilderFactory jobBuilderFactory;
-//    private final StepBuilderFactory stepBuilderFactory;
-//
-//
-//
-//    @Bean
-//    public Job job() {
-//        return jobBuilderFactory.get("batchJob")
-//                .start(step1())
-//                .next(step2())
-//                .build();
-//    }
-//
-//
-//    @Bean
-//    @JobScope
-//    public Step step1() {
-//        return stepBuilderFactory.get("step1")
-//                .<String, String>chunk(5) //<input, output>
-//                .reader(itemReader())
-//                .writer(new ItemWriter<String>() {
-//                    @Override
-//                    public void write(List items) throws Exception {
-//                        System.out.println("items = " + items);
-//                    }
-//                })
-//                .build();
-//    }
-//
-//    public FlatFileItemReader itemReader() {
-//        return new FlatFileItemReaderBuilder<Customer>()
-//                .name("flatFile")
-//                .resource(new FileSystemResource("D:\\develop\\springbatchlecture\\src\\main\\resources\\customer.txt"))
-//                .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
-//                .targetType(Customer.class)
-//                .linesToSkip(1)
-//                .fixedLength()
-//                .strict(false)
-//                .addColumns(new Range(1,5))
-//                .addColumns(new Range(6,9))
-//                .addColumns(new Range(10,11))
-//                .names("name", "year", "age")
-//                .build();
-//    }
-//
-////    @Bean
-////    public ItemReader itemReader() { // delimeted...
-////
-////        return new FlatFileItemReaderBuilder<Customer>()
-////                .name("flatFile")
-////                .resource(new ClassPathResource("/customer.txt"))
-////                .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
-////                .targetType(Customer.class)
-////                .linesToSkip(1)
-////                .delimited().delimiter(",") // 구분자 방식
-////                .names("name", "age", "year")
-////                .build();
-////
-////    }
-//
-//    @Bean
-//    public Step step2() {
-//        return stepBuilderFactory.get("step2")
-//                .tasklet((stepContribution, chunkContext) -> {
-//                    System.out.println(" step 2 has executed");
-//                    return RepeatStatus.FINISHED;
-//                }).build();
-//    }
-//
-//}
-//
+package io.springbatch.springbatchlecture.flatfile;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class FlatFilesConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(10)
+                .reader(customItemReader())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Customer> customItemReader() {
+        List<Customer> customers = Arrays.asList(
+                new Customer(1, "홍길동1", 30),
+                new Customer(2, "홍길동2", 31),
+                new Customer(3, "홍길동3", 32)
+
+        );
+
+        ListItemReader<Customer> reader = new ListItemReader<>(Collections.emptyList());
+
+        return reader;
+    }
+
+    @Bean
+
+    public ItemWriter<Customer> customItemWriter() {
+        return new FlatFileItemWriterBuilder<Customer>()
+                .name("flatFileWriter")
+                .resource(new FileSystemResource("C:\\lecture\\src\\main\\resources\\customer.txt"))
+                .append(true)
+                .formatted()
+                .format("%-2d%-4s%-2d")
+                .names(new String[]{"id", "name", "age"})
+                .build();
+
+    }
+}
+
